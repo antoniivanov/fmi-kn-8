4a3667805f8f16e67af5d39db77120c3ba3045dd
Unknown <anthvt@gmail.com>
2019-01-13 22:16:55 +0200

    x

--- up-ex13/binary_watch_with_bugs.cpp
+++ up-ex13/binary_watch_with_bugs.cpp
@@ -31,18 +31,19 @@ void get_all_combinations(char results[][6], int &results_index,
     }
 }
 
-void read_all_combinations_for_n_bits(char results[1024000][6], int *result_size, int num) {
+void read_all_combinations_for_n_bits(char results[1024000][6], int *result_size, int number_bits_set) {
     int results_size = 0;  // set initial size
     int bits_list[10] = {1, 2, 4, 8, 1, 2, 4, 8, 16, 32};
-    get_all_combinations(results, results_size, 0, 0, bits_list, 0, 10, num);
+    get_all_combinations(results, results_size, 0, 0, bits_list, 0, 10, number_bits_set);
 }
 
-void print_results(char result[1024000][6], int size) {
+void print_results(char results[1024000][6], int size) {
     for (int i = 0; i < size; ++i) {
-        std::cout << result << ", ";
+        std::cout << results[i] << ", ";
     }
     std::cout << std::endl;
 }
+
 
 int main() {
     int num;
